486413ec0058bae60491bfa6f0cdc8fe8e30a65c
cyberapp18 <cyberapp18@gmail.com>
2025-08-12 23:18:42 +0900

    Delete otayori_0812 directory

--- otayori_0812/services/mockData.ts
+++
@@ -1,7 +0,0 @@
-import { Notice, Task } from '../types';
-
-// The application now starts with no mock data.
-// All notices and tasks will be created from user uploads.
-export const MOCK_NOTICES: Notice[] = [];
-
-export const MOCK_TASKS: Task[] = [];
